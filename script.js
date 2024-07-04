73a43fb2944618347bbadc71bbcef4cc3d6b383a
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-04 22:19:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+ let text1 = document.querySelectorAll(".code");
+        text1.forEach((input, index) => {
+            input.addEventListener("input", () => {
+                if (input.value.length == 1) {
+                    if (text1[index + 1]) {
+                        text1[index + 1].focus();
+                    }
+                }
+            });
+            input.addEventListener("keydown", (event) => {
+                if (event.key == "Backspace" && input.value.length == 0) {
+                    if (text1[index - 1]) {
+                        text1[index - 1].focus();
+                    }
+                }
+            });
+        })
